e8399be932e8d339b81f4113861b0070c2cd6dda
dbmdbm <icp@o2.pl>
2021-02-19 22:20:29 +0100

    Initial commit

---
+++ src/main/java/switchStatement.java
@@ -0,0 +1,19 @@
+public class switchStatement {
+
+    public static void main(String[] args) {
+       int value1 = 1;
+       if (value1 ==1) {
+           System.out.println("Value was 1");
+       } else if (value1 ==2) {
+           System.out.println("Value was 2");
+       } else{
+           System.out.println("Was not 1 or 2");
+       }
+
+       int switchValue = 1;
+       switch (switchValue){
+           
+       }
+
+    }
+}
